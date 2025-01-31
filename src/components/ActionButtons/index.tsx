b933a18440c293509fe15f5d3cc4ec82e694afe1
daseul <7581058@gmail.com>
2025-01-31 23:55:03 +0900

    ✨ Feat: 이전,다음,제출 이동 구현

--- src/components/ActionButtons/index.tsx
+++ src/components/ActionButtons/index.tsx
@@ -1,11 +1,20 @@
+import { useNavigate } from 'react-router'
+
 import { StepProps } from '../QuestionBox'
 
 const ActionButtons = ({ questionsLength, step }: StepProps) => {
+  const navigate = useNavigate()
   const isLast = questionsLength - 1 === step
   return (
     <div>
-      {step === 0 || <button>이전</button>}
-      {isLast ? <button>제출</button> : <button>다음</button>}
+      {step === 0 || (
+        <button onClick={() => navigate(`${step - 1}`)}>이전</button>
+      )}
+      {isLast ? (
+        <button onClick={() => navigate('/done')}>제출</button>
+      ) : (
+        <button onClick={() => navigate(`${step + 1}`)}>다음</button>
+      )}
     </div>
   )
 }
