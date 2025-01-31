32f07976193313b789d4471c41740102d1e1fb9a
daseul <7581058@gmail.com>
2025-01-31 23:34:23 +0900

    ✨ Feat: 라우터 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { Route, Routes } from 'react-router'
+
+import CompletionPage from './pages/CompletionPage'
+import SurveyPage from './pages/SurveyPage'
+
 function App() {
-  return <></>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/survey/:surveyId/:step" element={<SurveyPage />} />
+        <Route path="/done" element={<CompletionPage />} />
+      </Routes>
+    </div>
+  )
 }
 
-export default App;
+export default App
