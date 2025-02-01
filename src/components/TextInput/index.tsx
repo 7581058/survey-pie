8ba3e688480a6ff495fbc3ff92af618c0d40b0f3
daseul <7581058@gmail.com>
2025-02-01 23:42:17 +0900

    ✨ Feat: text input placeholder 적용

--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -1,15 +1,29 @@
+import styled from 'styled-components'
+
 import { AnswerProps } from '../QuestionBox'
 
-const TextInput = ({ answer, setAnswer }: AnswerProps) => {
+const TextInput = ({ answer, setAnswer, options }: AnswerProps) => {
   return (
-    <input
+    <TextInputStyle
       type="text"
       value={answer}
       onChange={(e) => {
         setAnswer(e.target.value)
       }}
+      placeholder={options.placeholder}
     />
   )
 }
 
 export default TextInput
+
+const TextInputStyle = styled.input`
+  border: 1px solid #e0e0e0;
+  box-sizing: border-box;
+  border-radius: 5px;
+  padding: 12px 18px;
+
+  &::placeholder {
+    color: #bdbdbd;
+  }
+`
