6a585b04c3733c373ca8124b63acab5e5a3da824
daseul <7581058@gmail.com>
2025-02-12 00:13:56 +0900

    💄 Style: title 스타일링

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -1,8 +1,27 @@
+import styled from 'styled-components'
+
 interface TitleProps {
   children: React.ReactNode
 }
 const Title = ({ children }: TitleProps) => {
-  return <h1>{children}</h1>
+  return (
+    <TitleWrap>
+      <span>Q.</span>
+      {children}
+    </TitleWrap>
+  )
 }
 
 export default Title
+
+const TitleWrap = styled.h1`
+  margin: 0;
+  color: #121111;
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 28px;
+
+  span {
+    margin-right: 12px;
+  }
+`
