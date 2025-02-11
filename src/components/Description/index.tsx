5371ae8a2b1032f50835f32b9457ac9c6b53d4cc
daseul <7581058@gmail.com>
2025-02-12 00:15:00 +0900

    💄 Style: description 스타일링

--- src/components/Description/index.tsx
+++ src/components/Description/index.tsx
@@ -1,8 +1,13 @@
-interface DescriptionProps {
-  children: React.ReactNode
-}
-const Description = ({ children }: DescriptionProps) => {
-  return <h4>{children}</h4>
-}
+import styled from 'styled-components'
 
+const Description = styled.h4`
+  margin: 0;
+  font-size: 18px;
+  line-height: 21px;
+  color: #463c3c;
+  margin-top: 16px;
+  margin-bottom: 60px;
+  margin-left: 38px;
+  font-weight: normal;
+`
 export default Description
