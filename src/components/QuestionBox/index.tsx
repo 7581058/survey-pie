0c1184f94382b95f134897b840c3d97b52652b28
daseul <7581058@gmail.com>
2025-02-13 00:29:02 +0900

    ✨ Feat: question box 커스텀훅 적용

--- src/components/QuestionBox/index.tsx
+++ src/components/QuestionBox/index.tsx
@@ -1,39 +1,16 @@
-import { useParams } from 'react-router'
-import { useRecoilState, useRecoilValue } from 'recoil'
 import styled from 'styled-components'
 
-import { QustionOptionsType } from '../../mocks/questions'
-import { answersAtom } from '../../stores/answers/atom'
-import { questionsAtom } from '../../stores/questions/atom'
+import { useCurrentAnswer } from '../../hooks/useCurrentAnswer'
+import { useCurrentQuestion } from '../../hooks/useCurrentQuestion'
 import ActionButtons from '../ActionButtons'
 import Body from '../Body'
 import Description from '../Description'
 import Title from '../Title'
 
-// * 임시 타입
-export interface AnswerProps {
-  answer: any
-  setAnswer: any
-  options?: QustionOptionsType | null
-}
-
 const QuestionBox = () => {
-  const params = useParams()
-  const step = Number(params.step)
-
-  const questions = useRecoilValue(questionsAtom)
-  const [answers, setAnswers] = useRecoilState(answersAtom)
-
-  const question = questions[step]
-  const answer = answers[step]
-  const setAnswer = (newAnswer) => {
-    setAnswers((answers) => {
-      const newAnswers = [...answers]
-      newAnswers[step] = newAnswer
-      return newAnswers
-    })
-  }
-
+  const [answer, setAnswer] = useCurrentAnswer()
+  const question = useCurrentQuestion()
+  if (!question) return null
   return (
     <QuestionBoxWrap>
       <Title>{question.title}</Title>
