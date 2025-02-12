1ac8df7a82156c2dd8d4b0bdcb322f1071544a1d
daseul <7581058@gmail.com>
2025-02-13 00:39:48 +0900

    ✨ Feat: current question 커스텀훅 api요청 추가

--- src/hooks/useCurrentQuestion.ts
+++ src/hooks/useCurrentQuestion.ts
@@ -1,10 +1,21 @@
-import { useRecoilValue } from 'recoil'
+import axios from 'axios'
+import { useEffect } from 'react'
+import { useRecoilState } from 'recoil'
 
 import { questionsAtom } from '../stores/questions/atom'
 import { useStep } from './useStep'
+import { useSurveyId } from './useSurveyId'
 
 export const useCurrentQuestion = () => {
   const step = useStep()
-  const questions = useRecoilValue(questionsAtom)
+  const surveyId = useSurveyId()
+  const [surveyData, setSurveyData] = useRecoilState(questionsAtom)
+  const questions = surveyData?.questions || []
+
+  useEffect(() => {
+    axios.get(`http://localhost:3001/surveys/${surveyId}`).then((res) => {
+      setSurveyData(res.data)
+    })
+  }, [setSurveyData, surveyId])
   return questions[step]
 }
