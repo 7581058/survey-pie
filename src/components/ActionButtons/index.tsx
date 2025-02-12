0628689aec88a20ba46e0972699248966f74893c
daseul <7581058@gmail.com>
2025-02-13 01:07:53 +0900

    🐛 Fix: action button 커스텀 prop 문제 해결

--- src/components/ActionButtons/index.tsx
+++ src/components/ActionButtons/index.tsx
@@ -1,6 +1,7 @@
+import isPropValid from '@emotion/is-prop-valid'
 import { useNavigate } from 'react-router'
 import { useRecoilValue } from 'recoil'
-import styled from 'styled-components'
+import styled, { StyleSheetManager } from 'styled-components'
 
 import { useStep } from '../../hooks/useStep'
 import { questionsLengthSelector } from '../../stores/questions/questionsLengthSelector'
@@ -13,22 +14,24 @@ const ActionButtons = () => {
   const isLast = questionsLength - 1 === step
 
   return (
-    <ActionButtonsWrap>
-      {step === 0 || (
-        <Button styletype="SECONDARY" onClick={() => navigate(`${step - 1}`)}>
-          이전
-        </Button>
-      )}
-      {isLast ? (
-        <Button styletype="PRIMARY" onClick={() => navigate('/done')}>
-          제출
-        </Button>
-      ) : (
-        <Button styletype="PRIMARY" onClick={() => navigate(`${step + 1}`)}>
-          다음
-        </Button>
-      )}
-    </ActionButtonsWrap>
+    <StyleSheetManager shouldForwardProp={(prop) => isPropValid(prop)}>
+      <ActionButtonsWrap>
+        {step === 0 || (
+          <Button styletype="SECONDARY" onClick={() => navigate(`${step - 1}`)}>
+            이전
+          </Button>
+        )}
+        {isLast ? (
+          <Button styletype="PRIMARY" onClick={() => navigate('/done')}>
+            제출
+          </Button>
+        ) : (
+          <Button styletype="PRIMARY" onClick={() => navigate(`${step + 1}`)}>
+            다음
+          </Button>
+        )}
+      </ActionButtonsWrap>
+    </StyleSheetManager>
   )
 }
 
