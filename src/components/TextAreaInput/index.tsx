bc9be648b927334850ba312ec3ee3505b29167a8
daseul <7581058@gmail.com>
2025-02-19 01:21:32 +0900

    ✨ Feat: input 최대 입력 글자수 제한 구현

--- src/components/TextAreaInput/index.tsx
+++ src/components/TextAreaInput/index.tsx
@@ -14,6 +14,7 @@ const TextAreaInput = ({
         setAnswer(e.target.value)
       }}
       placeholder={options?.placeholder}
+      {...(options?.max && { maxLength: options.max })}
     />
   )
 }
--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -14,6 +14,13 @@ const TextInput = ({
         setAnswer(e.target.value)
       }}
       placeholder={options?.placeholder}
+      //일반적으로는 maxLength={options?.max}
+      //하지만 undefined일때도 동작함
+      //값이 있을때만 하고싶다면
+      //undefined가 아닐때 maxLength를 넣어주고
+      //undefined이라면 아무것도 분해가 안돼서 없는취급됨
+      //컴포넌트 props 조건적으로 전달할 때 유용
+      {...(options?.max && { maxLength: options.max })}
     />
   )
 }
